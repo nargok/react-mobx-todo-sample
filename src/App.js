dba7679dd7f94b657a0ecc1354b137dc50ec4a16
nargok <nargok@gmail.com>
2019-09-07 20:35:32 +0900

    mobx actions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { types, getSnapshot } from 'mobx-state-tree';
 
-const Todo = types.model({
-  name: types.optional(types.string, ""),
-  done: types.optional(types.boolean, false),
-})
+const Todo = types
+  .model({
+    name: types.optional(types.string, ""),
+    done: types.optional(types.boolean, false),
+  })
+  .actions(self => ({
+    setName(newName) {
+      self.name = newName
+    },
+
+    toggle() {
+      self.done = !self.done
+    }
+  }))
 
 const User = types.model({
   name: types.optional(types.string, ""),
 })
 
-const RootStore = types.model({
-  users: types.map(User),
-  todos: types.optional(types.map(Todo), {}),
-})
+const RootStore = types
+  .model({
+    users: types.map(User),
+    todos: types.optional(types.map(Todo), {}),
+  })
+  .actions(self => ({
+    addTodo(id, name) {
+      self.todos.set(id, Todo.create({ name }))
+    }
+  }))
 
 const store = RootStore.create({
   users: {}
@@ -24,6 +40,12 @@ const eat = Todo.create({ name: "eat" })
 
 console.log("john:", getSnapshot(john))
 console.log("Eat TODO:", getSnapshot(eat))
+
+store.addTodo(1, "Eat a cake")
+store.todos.get(1).toggle()
+
+console.log(getSnapshot(store))
+
 
 function App() {
   return (
