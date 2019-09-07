c9be9bf74a94561decc6061558240cd49d650d01
nargok <nargok@gmail.com>
2019-09-07 20:25:44 +0900

    create model

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { types, getSnapshot } from 'mobx-state-tree';
+
+const Todo = types.model({
+  name: types.optional(types.string, ""),
+  done: types.optional(types.boolean, false),
+})
+
+const User = types.model({
+  name: types.optional(types.string, ""),
+})
+
+const RootStore = types.model({
+  users: types.map(User),
+  todos: types.optional(types.map(Todo), {}),
+})
+
+const store = RootStore.create({
+  users: {}
+})
+
+const john = User.create()
+const eat = Todo.create({ name: "eat" })
+
+console.log("john:", getSnapshot(john))
+console.log("Eat TODO:", getSnapshot(eat))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
