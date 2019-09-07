29d7ece0b95506a693e4c5f2ccdf4b3117707381
nargok <nargok@gmail.com>
2019-09-07 21:23:47 +0900

    display store in console

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { values } from 'mobx';
+import { getSnapshot } from 'mobx-state-tree';
 
 let id = 1;
 const randomId = () => ++id;
 
-const App = observer(props => (
-  <>
-    <button onClick={e => props.store.addTodo(randomId(), "New Task")}>
-      Add Task
-    </button>
-    {
-      values(props.store.todos).map(todo => (
-        <div key={todo.id}>
-          <input
-            type="checkbox"
-            checked={todo.done}
-            onChange={() => todo.toggle()}
-          />
-          <input
-            type="text"
-            value={todo.name}
-            onChange={e => todo.setName(e.target.value)}
-          />
-        </div>
-      ))
-    }
-  </>
-))
+const App = observer(props => {
+  console.log(getSnapshot(props.store))
+  return (
+    <>
+      <button onClick={e => props.store.addTodo(randomId(), "New Task")}>
+        Add Task
+      </button>
+      {
+        values(props.store.todos).map(todo => (
+          <div key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={() => todo.toggle()}
+            />
+            <input
+              type="text"
+              value={todo.name}
+              onChange={e => todo.setName(e.target.value)}
+            />
+          </div>
+        ))
+      }
+    </>
+)})
 
 export default App;
